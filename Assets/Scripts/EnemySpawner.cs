2248aec7b6d3571c04535b693b0feff36dc77afb
EfeSimsek1 <efe.simsek.ca@gmail.com>
2025-11-12 11:35:36 -0500

    removed unused variable in EnemySpawner

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -8,7 +8,6 @@ using Random = UnityEngine.Random;
 
 public class EnemySpawner : MonoBehaviour
 {
-    [SerializeField] private GameObject[] enemyPrefabs;
     [SerializeField] private WaveInfo[] waves;
 
     [Header("Attributes")]
